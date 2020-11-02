608484cfeb0bee8471728ce3b23fd481339ff634
maw <maw@maw.de>
2020-11-02 20:45:30 +0100

    fix github bot (auth and response handling), replace (u)datetime with time-based implementation

--- demo/main.py
+++ demo/main.py
@@ -5,7 +5,12 @@ from config import github_config
 
 import utelegram
 import network
-import utime
+#import utime
+#import ujson
+import urequests
+import ntptime 
+import time as _time
+import base64
 
 def wlan_connect(ssid, password):
     import network
@@ -17,6 +22,16 @@ def wlan_connect(ssid, password):
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
+
+def padstr(num):
+    t = str(num)
+    if num < 10: t = '0' + t
+    return t
+
+def get_timestr():
+    t = _time.time()
+    y, m, d, hh, mm, ss, weekday, jday = _time.localtime(t)
+    return str(y) + '-' + padstr(m) + '-' + padstr(d), padstr(hh) + ':' + padstr(mm)
 
 
 # sta_if = network.WLAN(network.STA_IF)
@@ -50,27 +65,30 @@ def reply_btc(message):
 def store_github(message):
     print(message)
     cmd_index = message['message']['text'].index(' ')
-    text = message['message']['text'][cmd_index:]
-    from datetime import datetime
-    today_str = datetime.today().strftime('%Y-%m-%d')
+    text = message['message']['text'][(cmd_index+1):]
+    today_str, time_str = get_timestr()
+    #from datetime import datetime
+    #today_str = datetime.today().strftime('%Y-%m-%d')
+    
     todays_path = 'daily_notes/' + today_str + '.md'
+    #todays_path = 'daily_notes/demo.txt'
     print('storing: ' + text + ', @' + todays_path)
     
     headers = {
+        'User-Agent': 'esp32-github-bot',
         'Accept': 'application/vnd.github.v3+json',
-        'Authentication': 'Token ' + github_config['token']
+        'Authorization': 'token ' + github_config['token']
     }
     contents_path = github_config['api'] + '/repos/' + github_config['owner'] + '/' + github_config['repo'] + '/contents/' + todays_path
+    print('remote path: ' + contents_path)
     resp_exists = urequests.get(contents_path, headers=headers)
 
-    import base64
     new_content = None
     old_sha = None
-    if resp_exists.status_code > 400:
-        #create new
-        new_content = base64.b64encode(bytes(text, 'utf-8'))
-    else:
-        #update existing
+    print('status_code: ' + str(resp_exists.status_code))
+    if resp_exists.status_code > 400: #create new
+        new_content = base64.b64encode(bytes(time_str + '\r\n' + text, 'utf-8'))
+    else: #update existing
         resp_json = resp_exists.json() 
         if 'type' in resp_json:
             if resp_json['type'] != 'file':
@@ -78,7 +96,8 @@ def store_github(message):
         else:
             raise ValueError('Unexpected value: ' + resp_json)
         if 'content' in resp_json:
-            new_content = base64.b64decode(resp_json['content']) + '\r\n' + datetime.today().strftime('%h:%m') + '\r\n' + text
+            new_content = base64.b64decode(resp_json['content']) + '\r\n\r\n' + time_str + '\r\n' + text
+            print("new content: " + str(new_content))
             new_content = base64.b64encode(bytes(new_content, 'utf-8'))
         else:
             raise ValueError('Missing content: ' + resp_json)
@@ -93,29 +112,36 @@ def store_github(message):
     }
     if old_sha is not None:
         query_updates['sha'] = old_sha
-    urequests.put(contents_path, json=query_updates, headers=headers).json()
+    resp_put = urequests.put(contents_path, json=query_updates, headers=headers).json()
+    print(resp_put)
+    #print(resp_put.content)
+    commit_url = resp_put['commit']['html_url']
+    print("commited to: " + commit_url)
+    #return 'OK'
+    bot.send(message['message']['chat']['id'], commit_url)
 
 wlan_connect(ssid=wifi_config['ssid'], password=wifi_config['password'])
+ntptime.settime()
 
 debug = True
 
-if debug:
-    test_msg = {
-        'message': {
-            'text': '/put hello world'
-        }
-    }
-    store_github(test_msg)
-else:
-    #if sta_if.isconnected():
-    bot = utelegram.ubot(utelegram_config['token'])
-    #bot.register('/ping', reply_ping)
-    bot.register('/btc', reply_btc)
-    bot.register('/put', store_github)
-    bot.set_default_handler(get_message)
+#if debug:
+#    test_msg = {
+#        'message': {
+#            'text': '/put https://github.com\r\nhello world'
+#        }
+#    }
+#    store_github(test_msg)
+#else:
 
-    print('BOT LISTENING')
-    bot.listen()
-    #else:
-    #    print('NOT CONNECTED - aborting')
+#if sta_if.isconnected():
+bot = utelegram.ubot(utelegram_config['token'])
+#bot.register('/ping', reply_ping)
+bot.register('/btc', reply_btc)
+bot.register('/put', store_github)
+bot.set_default_handler(get_message)
 
+print('BOT LISTENING')
+bot.listen()
+#else:
+#    print('NOT CONNECTED - aborting')
